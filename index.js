c2b711425f1298cadab723413b3b68f158517048
Dani Sadorf <dani.noelani@gmail.com>
2017-11-16 17:41:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if string.toLowerCase() === lowercase
+  console.log("I can't hear you!")
+}
